9614fba902f5e40fb59be8ce3c215d22b1326ae0
Lían König <lk.liankonig@gmail.com>
2022-05-07 16:05:46 -0300

    changing the baseUrl for the cloud

--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'https://lk-bankline-api.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
